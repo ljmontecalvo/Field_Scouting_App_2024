cb329b57d00eb285f0d1671e593007e82ebd67c8
Landon Montecalvo <115672301+ljmontecalvo@users.noreply.github.com>
2024-01-20 16:29:07 -0500

    Made Super Object for Loading
    Created a super object that will be used for loading. Profile created, not yet programmed yet, DO THAT NEXT!!!

--- Assets/Scripts/Dashboard.cs
+++ Assets/Scripts/Dashboard.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class Dashboard : MonoBehaviour
 {
+    public GameObject superObjectPrefab;
+    
+    private void Start()
+    {
+        if (GameObject.FindGameObjectWithTag("Super Object") == null)
+        {
+            Instantiate(superObjectPrefab);
+        }
+    }
+    
     public void StartScout()
     {
         // TODO: Check that fields have been filled out.
---
+++ Assets/Scripts/SuperObject.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using Object = System.Object;
+
+public class SuperObject : MonoBehaviour
+{
+    private static string FILE_PATH = Application.dataPath + "/Saves/";
+    
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        for (var i = 0; i < CheckForSaves(); i++)
+        {
+            
+        }
+    }
+
+    private int CheckForSaves()
+    {
+        var counter = 0;
+        while (true)
+        {
+            if (File.Exists(FILE_PATH + "/save" + counter + ".txt"))
+            {
+                counter++;
+            }
+            else break;
+        }
+
+        return counter;
+    }
+}
